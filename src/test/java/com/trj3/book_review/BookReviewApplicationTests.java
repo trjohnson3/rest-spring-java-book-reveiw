64ae63817e4da615b1a98b4a9a517c86500cdb01
Tom Johnson <trjohnson3@gmail.com>
2024-07-02 16:07:50 -0500

    initial setup

---
+++ src/test/java/com/trj3/book_review/BookReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trj3.book_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
